7d2019cce794411e7031f8c29023ff2c69d45381
Andrea <andreademoja@gmail.com>
2017-09-13 10:46:57 +0200

    added footer in index added some project and description in yaml file

---
+++ my_site/spec/features/index_spec.rb
@@ -0,0 +1,16 @@
+
+it 'displays project list' do
+   expect(page).to have_css '.projects'
+   within '.projects' do
+     expect(page).to have_content 'Bmi calculator'
+     expect(page).to have_content 'FizzBuzz'
+   end
+ end
+
+it 'renders footer partial' do
+  expect(page).to have_selector 'footer'
+  within 'footer' do
+    expect(page).to have_content 'My Portfolio'
+    expect(page).to have_content 'Built using the awesome Middleman framework'
+  end
+end
